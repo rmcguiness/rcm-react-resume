d8ce708029a9997c6ea0f609c837c391b25c92b1
Ryan McGuiness <ryanmcguiness123@gmail.com>
2025-04-20 14:44:53 -0400

    fixed version mismatch and build errors

--- src/components/Sections/Resume/TimelineItem.tsx
+++ src/components/Sections/Resume/TimelineItem.tsx
@@ -1,8 +1,8 @@
 import {FC, memo} from 'react';
 
-import {TimelineItem} from '../../../data/dataDef';
+import type {TimelineItem} from '../../../data/dataDef';
 
-const TimelineItem: FC<{item: TimelineItem}> = memo(({item}) => {
+const TimelineItem: FC<{ item: TimelineItem }> = memo(({item}) => {
   const {title, date, location, content} = item;
   return (
     <div className="flex flex-col pb-8 text-center last:pb-0 md:text-left">
